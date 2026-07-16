2e4913d89e3908e288499ee08fd7d1e30b0dba7e
cheeptana yenlab <cheaptana.yen@rmutto.ac.th>
2026-07-16 12:46:56 +0700

    week02 commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,52 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+export default function profile(){
+    
+    return (
+        <>
+        <div className="max-w-sm mx-auto bg-white dark:bg-slate-800 rounded-2xl shadow-xl overflow-hidden mt-20 mb-6">
+  <div className="h-32 bg-gradient-to-r from-cyan-500 to-blue-500"></div>
+ 
+  <div className="relative flex flex-col items-center px-6 pb-6">
+    <div className="-mt-16 mb-4">
+      <img className="w-28 h-28 rounded-full border-4 border-white dark:border-slate-800 shadow-md object-cover"
+           src='/images/thongsin.jpg'
+           alt="..." />
+    </div>
+   
+    <h3 className="text-xl font-bold text-slate-800 dark:text-white">Thongsin Khumbuala</h3>
+    <p className="text-sm text-blue-600 dark:text-blue-400 font-medium">movie</p>
+    <p className="text-sm text-slate-500 dark:text-slate-400 text-center mt-3">
+      สาขา เทคโนโลยีสารสนเทศ คณะบริหารธุรกิจและเทคโนโลยีสารสนเทศ มหาลัยราชมงคลตะวันออกเขตจักรพงษภูวนารถ
+    </p>
+
+    <div className="mt-6 flex gap-3">
+      <a className="px-5 py-2 bg-blue-600 text-white text-sm font-semibold rounded-lg shadow-md hover:bg-blue-700 transition"
+         href="mailto:thongsin.khu@rmutto.ac.th"
+         target="_black">
+        E-mail
+      </a>
+      <a className="px-5 py-2 border border-slate-300 text-slate-700 dark:text-slate-300 text-sm font-semibold rounded-lg hover:bg-slate-50 dark:hover:bg-slate-700 transition"
+        href="https://github.com/moneyfc12-sys/sin-next-app.git"
+        target="_blank">
+        Github
+      </a>
+    </div>
+
+    <div className="flex mt-6 space-x-4">
+      <a href="#" className="text-slate-400 hover:text-blue-500">
+        <img className="w-10 h-10 rounded-full border border-white dark:border-slate-800 shadow-md object-cover"
+           src='\images\instargram.jpg' 
+           alt="" />
+      </a>
+      {/* Display the facebook icon or logo. */}
+      {/* Display the instagram icon or logo. */}
+      {/* Display the github icon or logo. */}
+    </div>
+  </div>
+</div>
+<Footer></Footer>
+</>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,25 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Welcome To sin Web App.
           </h1>
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://www.minecraft.net/en-us"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/images/minecraft.jpg"
+              alt="minecraft logomark"
+              width={25}
+              height={25}
+            />
+            minecraft Now !!
+          </a>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            play now{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
@@ -48,7 +63,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Deploy Now
+            play Now !!
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
@@ -63,3 +78,4 @@ export default function Home() {
     </div>
   );
 }
+
