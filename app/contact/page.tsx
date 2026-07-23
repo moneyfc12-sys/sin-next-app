802820a69339c762d68f4d60dbc5dd9c4cfc0c0a
cheeptana yenlab <cheaptana.yen@rmutto.ac.th>
2026-07-23 12:42:44 +0700

    week03 commit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,4 @@
-import Header from "../components/header";
+import Header from "../components/footer";
 import Footer from "../components/footer";
 
 export default function profile(){
---
+++ app/data/toDolist.tsx
@@ -0,0 +1,23 @@
+export const toDoList =[
+    {
+        title: "เรียนวิชา web programming",
+        des: "React (Next.js), Node.js และ Firebase firestore",
+        data_added: "23/07/2569",
+        author: "Beritokai",
+        status: true
+    },
+     {
+       title: "ออกกำลังกาย",
+        des: "ออกกำลังกายที่สนาม มธ.",
+        data_added: "23/07/2569",
+        author: "Beritokai",
+        status: true
+    },
+     {
+        title: "กินหมูกะทะ",
+        des: "กินหมูกะทะหลัง",
+        data_added: "23/07/2569",
+        author: "Beritokai",
+        status: true
+    }
+]; 
